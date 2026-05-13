3ae9b5067e1ff29bf05f8245c7f307033f70713d
ArdaKupeli <ardakupeli2000@gmail.com>
2026-05-14 01:09:26 +0300

    Refactor game.asm for clarity and structure

--- game.asm
+++ game.asm
@@ -1,14 +1,28 @@
 .data
 welcomeMsg:     .asciiz "\n=== MIPS SNAKE LITE ===\n"
 menuMsg:        .asciiz "\nMove: 1=Up  2=Down  3=Left  4=Right  5=Exit\nChoice: "
+
 rowMsg:         .asciiz "\nSnake position -> Row: "
 colMsg:         .asciiz "  Col: "
+
+foodRowMsg:     .asciiz "\nFood position  -> Row: "
+foodColMsg:     .asciiz "  Col: "
+
+scoreMsg:       .asciiz "\nScore: "
+eatMsg:         .asciiz "\nFood eaten! Score increased.\n"
+winMsg:         .asciiz "\nYou win! Final score reached.\n"
 gameOverMsg:    .asciiz "\nGame Over! Snake hit the wall.\n"
 exitMsg:        .asciiz "\nExiting game...\n"
 invalidMsg:     .asciiz "\nInvalid input. Try again.\n"
 
 snakeRow:       .word 2
 snakeCol:       .word 2
+
+foodRow:        .word 0
+foodCol:        .word 4
+
+score:          .word 0
+targetScore:    .word 3
 boardSize:      .word 5
 
 .text
@@ -21,7 +35,7 @@ main:
     syscall
 
 game_loop:
-    # Print current row
+    # Print snake row
     li $v0, 4
     la $a0, rowMsg
     syscall
@@ -30,13 +44,40 @@ game_loop:
     lw $a0, snakeRow
     syscall
 
-    # Print current col
+    # Print snake col
     li $v0, 4
     la $a0, colMsg
     syscall
 
     li $v0, 1
     lw $a0, snakeCol
+    syscall
+
+    # Print food row
+    li $v0, 4
+    la $a0, foodRowMsg
+    syscall
+
+    li $v0, 1
+    lw $a0, foodRow
+    syscall
+
+    # Print food col
+    li $v0, 4
+    la $a0, foodColMsg
+    syscall
+
+    li $v0, 1
+    lw $a0, foodCol
+    syscall
+
+    # Print score
+    li $v0, 4
+    la $a0, scoreMsg
+    syscall
+
+    li $v0, 1
+    lw $a0, score
     syscall
 
     # Print menu
@@ -49,7 +90,7 @@ game_loop:
     syscall
     move $t9, $v0       # $t9 = user choice
 
-    # Compare input
+    # Direction control
     li $t8, 1
     beq $t9, $t8, move_up
 
@@ -105,7 +146,7 @@ check_wall:
     bltz $t0, game_over
 
     # If row >= boardSize -> game over
-    slt $t6, $t0, $t5     # $t6 = 1 if row < boardSize
+    slt $t6, $t0, $t5
     beq $t6, $zero, game_over
 
     # Load col
@@ -115,10 +156,66 @@ check_wall:
     bltz $t1, game_over
 
     # If col >= boardSize -> game over
-    slt $t6, $t1, $t5     # $t6 = 1 if col < boardSize
+    slt $t6, $t1, $t5
     beq $t6, $zero, game_over
 
-    # If no wall collision, continue game
+    b check_food
+
+
+check_food:
+    # Check if snakeRow == foodRow
+    lw $t0, snakeRow
+    lw $t1, foodRow
+    bne $t0, $t1, game_loop
+
+    # Check if snakeCol == foodCol
+    lw $t2, snakeCol
+    lw $t3, foodCol
+    bne $t2, $t3, game_loop
+
+    # If both row and col match, food is eaten
+    li $v0, 4
+    la $a0, eatMsg
+    syscall
+
+    # score = score + 1
+    lw $t4, score
+    addi $t4, $t4, 1
+    sw $t4, score
+
+    # If score == targetScore, win
+    lw $t5, targetScore
+    beq $t4, $t5, win_game
+
+    # Move food based on score
+    li $t6, 1
+    beq $t4, $t6, set_food_second
+
+    li $t6, 2
+    beq $t4, $t6, set_food_third
+
+    b game_loop
+
+
+set_food_second:
+    # New food position: row = 4, col = 4
+    li $t7, 4
+    sw $t7, foodRow
+
+    li $t7, 4
+    sw $t7, foodCol
+
+    b game_loop
+
+
+set_food_third:
+    # New food position: row = 1, col = 1
+    li $t7, 1
+    sw $t7, foodRow
+
+    li $t7, 1
+    sw $t7, foodCol
+
     b game_loop
 
 
@@ -127,6 +224,15 @@ invalid_input:
     la $a0, invalidMsg
     syscall
     b game_loop
+
+
+win_game:
+    li $v0, 4
+    la $a0, winMsg
+    syscall
+
+    li $v0, 10
+    syscall
 
 
 game_over:
